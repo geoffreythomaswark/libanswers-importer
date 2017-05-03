460c56707da6bd51feb2bb521f287275a50e750d
Geoffrey T. Wark <deek9n@protonmail.ch>
2017-05-02 21:36:53 -0400

    Move to public repo

---
+++ frmMain.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace LA_Importer2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeSourceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSourceName = new System.Windows.Forms.TextBox();
+            this.txtSourcePath = new System.Windows.Forms.TextBox();
+            this.btnChooseSource = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(534, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.importToolStripMenuItem.Text = "Import";
+            this.importToolStripMenuItem.Click += new System.EventHandler(this.importToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeSourceToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // changeSourceToolStripMenuItem
+            // 
+            this.changeSourceToolStripMenuItem.Name = "changeSourceToolStripMenuItem";
+            this.changeSourceToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.changeSourceToolStripMenuItem.Text = "Change Source";
+            this.changeSourceToolStripMenuItem.Click += new System.EventHandler(this.changeSourceToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewHelpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // viewHelpToolStripMenuItem
+            // 
+            this.viewHelpToolStripMenuItem.Name = "viewHelpToolStripMenuItem";
+            this.viewHelpToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.viewHelpToolStripMenuItem.Text = "View Help";
+            this.viewHelpToolStripMenuItem.Click += new System.EventHandler(this.viewHelpToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(22, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source File Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(22, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Source File Path:";
+            // 
+            // txtSourceName
+            // 
+            this.txtSourceName.Location = new System.Drawing.Point(25, 53);
+            this.txtSourceName.Name = "txtSourceName";
+            this.txtSourceName.ReadOnly = true;
+            this.txtSourceName.Size = new System.Drawing.Size(353, 20);
+            this.txtSourceName.TabIndex = 3;
+            // 
+            // txtSourcePath
+            // 
+            this.txtSourcePath.Location = new System.Drawing.Point(25, 100);
+            this.txtSourcePath.Name = "txtSourcePath";
+            this.txtSourcePath.ReadOnly = true;
+            this.txtSourcePath.Size = new System.Drawing.Size(353, 20);
+            this.txtSourcePath.TabIndex = 4;
+            // 
+            // btnChooseSource
+            // 
+            this.btnChooseSource.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnChooseSource.Location = new System.Drawing.Point(396, 53);
+            this.btnChooseSource.Name = "btnChooseSource";
+            this.btnChooseSource.Size = new System.Drawing.Size(121, 67);
+            this.btnChooseSource.TabIndex = 5;
+            this.btnChooseSource.Text = "Choose Source File";
+            this.btnChooseSource.UseVisualStyleBackColor = true;
+            this.btnChooseSource.Click += new System.EventHandler(this.btnChooseSource_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 146);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(509, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(12, 196);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.ReadOnly = true;
+            this.rtbOutput.Size = new System.Drawing.Size(510, 123);
+            this.rtbOutput.TabIndex = 7;
+            this.rtbOutput.Text = "";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnExit.Location = new System.Drawing.Point(427, 328);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(95, 36);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnImport.Location = new System.Drawing.Point(312, 328);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(95, 36);
+            this.btnImport.TabIndex = 9;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(12, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Output:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 376);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnChooseSource);
+            this.Controls.Add(this.txtSourcePath);
+            this.Controls.Add(this.txtSourceName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(550, 415);
+            this.MinimumSize = new System.Drawing.Size(550, 415);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LibAnswers Importer";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewHelpToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSourceName;
+        private System.Windows.Forms.TextBox txtSourcePath;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeSourceToolStripMenuItem;
+        private System.Windows.Forms.Button btnChooseSource;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,262 @@
+﻿/****************************** Module Header ******************************\
+Module Name:    frmMain.cs
+Project:        LA-Importer2
+Created by:     Nehru Becirovic
+Maintainer:     Geoff T. Wark
+Date Created:   Jan-09-2014
+Last Modified:  
+
+Copyright (c) 2014 Thomas G. Carpenter Library.
+
+This program facilitates the the loading of LibAnswers (Reference Analytics)
+data into a SQL database.
+
+This source is subject to the Microsoft Public License.
+See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL.
+All other rights reserved.
+
+THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+// TODO: make the progress bar work
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+using System.Management.Automation;
+
+namespace LA_Importer2
+{
+    public partial class frmMain : Form
+    {
+        private string connection_info = null;
+        private string db_table = null;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            MinimizeBox = false;
+            MaximizeBox = false;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader sr = File.OpenText(
+                    "C:\\Users\\" + Environment.UserName
+                    + "\\AppData\\Roaming\\LA-Importer2\\config");
+
+                connection_info = sr.ReadLine();
+                db_table = sr.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                MessageBox.Show("No config file found!\n\n"
+                    + "Please create one named 'config' at "
+                    + "%appdata%/LibAnswers/.\n"
+                    + "(1st line = connection string; 2nd line = database table)",
+                    "ERROR",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                this.Close();
+            }
+        }
+
+        private void btnChooseSource_Click(object sender, EventArgs e)
+        {
+            ChooseSourceFile();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            ImportData(txtSourcePath.Text);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ChooseSourceFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtSourceName.Text = ofd.SafeFileName;
+                txtSourcePath.Text = ofd.FileName;
+            }
+        }
+        
+        private void ImportData(string csv_file)
+        {
+            if (txtSourcePath.Text != "")
+            {
+                // a temp file is used here due to user permission issues
+                string temp_csv = "C:\\Users\\" + Environment.UserName
+                    + "\\AppData\\Roaming\\LA-Importer2\\delete_me.csv";
+                DataTable dt = null;
+
+                rtbOutput.Text = null;
+                rtbOutput.Text += "Now importing...\n\n";
+
+                ScrubCSV(csv_file, temp_csv);
+                dt = ParseCSV(temp_csv);
+
+                try
+                {
+                    SqlConnection cn = new SqlConnection(connection_info);
+                    cn.Open();
+                    using (SqlBulkCopy bulk = new SqlBulkCopy(cn))
+                    {
+                        bulk.DestinationTableName = db_table;
+                        bulk.WriteToServer(dt);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                    rtbOutput.Text += (ex + "\n");
+                    return;
+                }
+                rtbOutput.Text += "Import complete! Thank you.";
+            }
+            else
+            {
+                MessageBox.Show("No source file selected!",
+                    "ERROR",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+            }
+        }
+
+        private void ScrubCSV(string csv_file, string temp_csv)
+        {
+            PowerShell ps = PowerShell.Create();
+            string script = "Import-Csv " + csv_file + " | select id,date,time,"
+                    + "\"entered by\",\"where you received the question\","
+                    + "\"question type\",\"method used to answer\" | Export-Csv "
+                    + "-Path " + temp_csv + " -NoTypeInformation";
+
+            try
+            {
+                ps.AddScript(script);
+                ps.Invoke();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                rtbOutput.Text += (ex + "\n");
+                return;
+            }
+            finally
+            {
+                ps.Dispose();
+            }
+        }
+
+        private DataTable ParseCSV(string temp_csv)
+        {
+            StreamReader sr = null;
+            string currentLine = null;
+            DataTable dt = new DataTable();
+
+            try
+            {
+                sr = File.OpenText(temp_csv);
+                int i = 0;
+                while ((currentLine = sr.ReadLine()) != null)
+                {
+                    currentLine = currentLine.Replace("\",\"", "|");
+                    string[] data = currentLine.Split('|');
+                    data[0] = data[0].TrimStart('"');
+                    data[6] = data[6].TrimEnd('"');
+
+                    if (i == 0)
+                    {
+                        foreach (var item in data)
+                        {
+                            dt.Columns.Add(new DataColumn());
+                        }
+                    }
+                    else
+                    {
+                        DataRow row = dt.NewRow();
+                        row.ItemArray = data;
+                        dt.Rows.Add(row);
+                    }
+                    i++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                rtbOutput.Text += (ex + "\n");
+                return null;
+            }
+            finally
+            {
+                sr.Close();
+            }
+
+            try
+            {
+                File.Delete(temp_csv);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                rtbOutput.Text += (ex + "\n");
+            }
+
+            return dt;
+        }
+
+        // menu strip
+        private void importToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ImportData(txtSourcePath.Text);
+        }
+
+        private void changeSourceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChooseSourceFile();
+        }
+
+        private void viewHelpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // TODO: improve this
+            MessageBox.Show(
+                "Please contact geoff.wark@unf.edu if you are having trouble.",
+                "LibAnswers Importer Help",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Question);
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // TODO: improve this
+            MessageBox.Show("LibAnswers Importer\n"
+                + "Version 0.02\n\n"
+                + "Copyright (c) 2014 Thomas G. Carpenter Library.\n"
+                + "This source is subject to the Microsoft Public License.\n"
+                + "See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL.\n"
+                + "All other rights reserved.",
+                "About LibAnswers Importer",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
